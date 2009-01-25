24d26cd7a6852d542199a455ff114a92e48f3ebf
Stuart Halloway <stu@thinkrelevance.com>
2009-01-25 11:27:31 -0500

    varargs on end of task creation possible hook for adding resources

--- lancet.clj
+++ lancet.clj
@@ -29,10 +29,15 @@
 	   (.getPropertyDescriptors 
 	    (Introspector/getBeanInfo (class inst))))))
 
+(defn get-property-class [write-method]
+  (first (.getParameterTypes write-method)))
+
 (defn set-property! [inst prop value]
-  (let [pd (property-descriptor inst prop)]   
-    (throw-if (nil? pd) (str "No such property " prop)) 
-    (.invoke (. pd getWriteMethod) inst (into-array [value])))) 
+  (let [pd (property-descriptor inst prop)]
+    (throw-if (nil? pd) (str "No such property " prop))
+    (let [write-method (.getWriteMethod pd)
+	  dest-class (get-property-class write-method)]
+      (.invoke write-method inst (into-array [(coerce dest-class value)])))))
 
 (defn set-properties! [inst prop-map]
   (doseq [[k v] prop-map] (set-property! inst (name k) v))) 
@@ -42,9 +47,8 @@
     (throw-if (nil? task) (str "No task named " name))
     (doto task
       (.init)
-      (.setProject project)) 
-    (set-properties! task props) 
-    task))
+      (.setProject project)
+      (set-properties! props))))
 
 (defn runonce
  "Create a function that will only run once. All other invocations
@@ -79,10 +83,16 @@
 	    once-fn#))))
 
 (defmacro define-ant-task [clj-name ant-name]
-  `(defn ~clj-name [props#]
-     (let [task# (instantiate-task ant-project ~(name ant-name) props#)]
+  `(defn ~clj-name [& props#]
+     (let [task# (apply instantiate-task ant-project ~(name ant-name) props#)]
        (.execute task#)
        task#)))
+
+(defmacro define-bean-constructor [clj-name ant-name]
+  `(defn ~clj-name [props#]
+     (let [bean# (new ~ant-name)]
+       (set-properties! bean# props#)
+       bean#)))
 
 (defn task-names [] (map symbol (seq (.. ant-project getTaskDefinitions keySet))))
 
@@ -96,6 +106,13 @@
   `(do ~@(map (fn [n] `(define-ant-task ~(safe-ant-name n) ~n)) (task-names))))
 
 (define-all-ant-tasks)
+
+; one possible way to create non-task Ant stuff
+(define-bean-constructor files org.apache.tools.ant.types.resources.Files)
+
+; this would have to be wired to an Ant project
+; (define-bean-constructor fileset org.apache.tools.ant.types.FileSet)
+	   
 
 
 
