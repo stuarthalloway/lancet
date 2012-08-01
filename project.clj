01f3d89400d29ca512169823fd32d2ad5a465a4d
Pavel Repin <prepin@gmail.com>
2012-07-31 22:41:19 -0700

    Point at ant 1.8.4.
    Note that corresponding ant-nodeps is no longer in Central.

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject lancet "1.0.1"
   :description "Dependency-based builds, Clojure Style, with optional Ant underneath."
   :license {:name "MIT X11 License"}
-  :dependencies [[org.apache.ant/ant "1.7.1"]
-                 [org.apache.ant/ant-nodeps "1.7.1"]]
+  :dependencies [[org.apache.ant/ant "1.8.4"]]
   :eval-in-leiningen true)
