fac519eea9b3fdedb39f010b1fd530b522a65df6
Pavel Repin <prepin@gmail.com>
2012-07-31 22:57:12 -0700

    Add "-SNAPSHOT" suffix to the artifact version, for now.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lancet "1.0.1"
+(defproject lancet "1.0.2-SNAPSHOT"
   :description "Dependency-based builds, Clojure Style, with optional Ant underneath."
   :license {:name "MIT X11 License"}
   :dependencies [[org.apache.ant/ant "1.8.4"]]
